97621029538fdd6077919e71fc136da6380e6a17
Alexander Nesterov <nesterov.alexander@outlook.com>
2023-03-13 17:20:26 +0100

    separate common executor

--- src/plugins/intel_cpu/src/nodes/shuffle_channels.h
+++ src/plugins/intel_cpu/src/nodes/shuffle_channels.h
@@ -3,13 +3,13 @@
 //
 
 #pragma once
-
 #include <ie_common.h>
 #include <node.h>
 #include <string>
 #include <memory>
 #include <vector>
 #include "common/permute_kernel.h"
+#include "executors/shuffle_channels_list.hpp"
 
 namespace ov {
 namespace intel_cpu {
@@ -26,38 +26,18 @@ public:
     void createPrimitive() override;
     void execute(dnnl::stream strm) override;
     bool created() const override;
-
     void prepareParams() override;
-    struct ShuffleChannelsAttributes {
-        LayoutType layoutType;
-        int dataRank = 0;
-        int axis = 0;
-        int spatialRank = 0;
-        size_t group = 0lu;
-        size_t dataSize = 1lu;
-        VectorDims srcDims;
-        VectorDims srcBlockedDims;
-        size_t hash() const;
-        bool operator==(const ShuffleChannelsAttributes& rhs) const;
-    };
+    void addSupportedPrimDescFactory(const std::vector<PortConfigurator>& inPortConfigs,
+                                    const std::vector<PortConfigurator>& outPortConfigs,
+                                    impl_desc_type implType,
+                                    bool dynBatchSupport = false);
 
 protected:
     void executeDynamicImpl(dnnl::stream strm) override;
 
 private:
     ShuffleChannelsAttributes attrs;
-
-    struct ShuffleChannelsExecutor final {
-        ShuffleChannelsExecutor(const ShuffleChannelsAttributes& attrs);
-        void exec(const uint8_t* srcData, uint8_t* dstData, const int MB);
-        ~ShuffleChannelsExecutor() = default;
-
-    private:
-        std::unique_ptr<PermuteKernel> permuteKernel = nullptr;
-    };
-    using executorPtr = std::shared_ptr<ShuffleChannelsExecutor>;
-    executorPtr execPtr = nullptr;
-
+    ShuffleChannelsExecutorPtr execPtr = nullptr;
     bool supportDynamicBatch = false;
 };
 
